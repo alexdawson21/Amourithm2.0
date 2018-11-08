89972943de9d263b7728af83f7d890cf6836a5e1
Alex Dawson <alexanderdawson@hotmail.ca>
2018-11-08 16:38:45 -0500

    f

--- client/src/App.js
+++ client/src/App.js
@@ -27,28 +27,28 @@ class App extends Component {
     suitors:[],
   };
 
-  // componentDidMount() {
-  //   //request to load in the users profile, will be changed to be based on ID once synced with login
-  //     axios.get("api/profile")
-  //     .then(res=>this.setState({profile: res.data[0]}))
+  componentDidMount() {
+    //request to load in the users profile, will be changed to be based on ID once synced with login
+      axios.get("api/profile")
+      .then(res=>this.setState({profile: res.data[0]}))
 
-  //     //requesto to load potential matches. Is set to timeout so that user preferences can be loaded in.
-  //     //res.data should be an array of users matching the gender designation.
-  //     //res.data is assigned to this.state.suitors which is then passed into the suitors component as props. 
-  //     setTimeout(()=>{
-  //       if(this.state.profile.seeking === "male"){
-  //         axios.get("api/suitors/men")
-  //         .then(res=>this.setState({suitors: res.data}))
-  //       }
-  //       else if(this.state.profile.seeking === "female"){
-  //         axios.get("api/suitors/women")
-  //         .then(res=>this.setState({suitors: res.data}))
-  //       }
-  //     },2000)
-  //     setTimeout(()=>{
-  //       console.log(this.state.suitors)
-  //     },3000)
-  // }
+      //requesto to load potential matches. Is set to timeout so that user preferences can be loaded in.
+      //res.data should be an array of users matching the gender designation.
+      //res.data is assigned to this.state.suitors which is then passed into the suitors component as props. 
+      setTimeout(()=>{
+        if(this.state.profile.seeking === "male"){
+          axios.get("api/suitors/men")
+          .then(res=>this.setState({suitors: res.data}))
+        }
+        else if(this.state.profile.seeking === "female"){
+          axios.get("api/suitors/women")
+          .then(res=>this.setState({suitors: res.data}))
+        }
+      },2000)
+      setTimeout(()=>{
+        console.log(this.state.suitors)
+      },3000)
+  }
 
   render() {
     return (
